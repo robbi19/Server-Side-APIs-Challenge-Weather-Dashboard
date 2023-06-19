1db9aefa85962ccbb14b74f0d3c964b3af9bad48
rob <collinsduo1@gmail.com>
2023-06-18 20:17:07 -0500

    script and style

---
+++ script.js
@@ -0,0 +1,9 @@
+// need to have temp,city,humid,wind,uv//
+var currCity = $("#currentCity");
+var currTemp = $("#Temperature");
+var currHumidity = $("#humidity");
+var currwindSpeed = $("#windSpeed");
+var currUvIndex = $("#uvIndex");
+var searchHistory = $(".searchHistory");
+var searchCity = $(".input");
+var searchButton = $("#searchBtn");
